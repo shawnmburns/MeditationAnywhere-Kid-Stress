9a1066a675f653183bdcb67f57edb3516ede0396
Shawn Burns <shawn.m.burns@gmail.com>
2012-12-28 15:13:24 -0500

    Updated "more from us" link and icons.

--- src/com/meditatenowKidStress/api/MNCommon.java
+++ src/com/meditatenowKidStress/api/MNCommon.java
@@ -2,12 +2,12 @@ package com.meditatenowKidStress.api;
 
 public abstract class MNCommon {
 	
-	public static final String AppPackageName = "com.meditatenow";
+	public static final String AppPackageName = "com.meditatenowKidStress";
 	
 	public static final String UrlMeditationAnywhere = "http://www.meditationanywhere.com/";
 	public static final String UrlFAQ = "http://www.meditationanywhere.com/faq";
 	
-	public static final String UrlGetMoreFromUs = "https://play.google.com/store/apps/developer?id=meditationanywhere";
+	public static final String UrlGetMoreFromUs = "https://play.google.com/store/apps/developer?id=MeditationAnywhere";
 	public static final String UrlLikeUs = "https://www.facebook.com/MeditationAnywhereCommunity";
 	public static final String UrlFollowUs = "https://twitter.com/MedtatnAnywhere";
 	
